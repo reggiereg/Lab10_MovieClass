4da38597cd7e835668f14e7419ea2734bf131386
Reginald Richardson <youngreg21@gmail.com>
2020-01-31 07:28:23 -0500

    Lab 10 - Movie Class

---
+++ Movie.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_MovieApp
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+        private int intCategory;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        public int IntCategory
+        {
+            get { return intCategory; }
+            set { intCategory = value; }
+        }
+
+        public Movie(string title, string category, int intCategory)
+        {
+            this.title = title;
+            this.category = category;
+            this.intCategory = intCategory;
+        }
+
+        //public static string GetUserInput(string message)
+        //{
+        //    string userInput;
+        //    Console.WriteLine(message);
+        //    userInput = Console.ReadLine();
+
+        //    return userInput;
+        //}
+
+        //public static int GetUserInput(string message, int i)
+        //{
+        //    string userInput;
+        //    Console.WriteLine(message);
+        //    userInput = Console.ReadLine();
+        //    int value = ValidateUserInput(userInput, i);
+
+        //    return value;
+        //}
+
+        //public static int ValidateUserInput(string userInput, int categoryNum)
+        //{
+        //    int categoryOption;
+            
+        //    try
+        //    {
+        //        categoryOption = int.Parse(userInput);
+        //        //return categoryOption;
+        //    }
+
+        //    catch (FormatException)
+        //    {
+        //        return ValidateUserInput(GetUserInput("Could not parse your entry to an int.  Make sure you're entering in a number"), categoryNum);
+        //    }
+        //    if (categoryOption >= 1 && categoryOption <= categoryNum)
+        //    {
+        //        return categoryOption;
+        //    }
+        //    else
+        //    {
+        //        return ValidateUserInput(GetUserInput("Value is not a valid option.  Please select a number between 1 and {categoryNum}"), categoryNum);
+        //    }
+
+        //}
+
+        private static List<string> GetCategories(List<Movie> movies)
+        {
+            List<string> categories = new List<string>();
+            
+            foreach(Movie cat in movies)
+            {
+                if(!categories.Contains(cat.category))
+                {
+                    categories.Add(cat.category.ToString());
+                }
+            }
+
+            return categories;
+        }
+
+        public static void PrintCategories(List<Movie> movies)
+        {
+            List<string> categories = new List<string>();
+            
+            categories = GetCategories(movies);
+            
+            int i = 1;
+            Console.WriteLine("MOVIE CATEGORY LISTING");
+            Console.WriteLine("======================");
+            
+            foreach(string cat in categories)
+            {
+                Console.WriteLine($"{i}. {cat}");
+                i++;
+            }
+        }
+
+        public static string PrintMovies( int maxEntry, int category, List<Movie> movies)
+        {
+           
+            string Continue;
+            if (category <= maxEntry) 
+            {
+                Console.WriteLine($"AVAILABLE MOVIES FOR THE SELECTED CATEGORY: ");
+                Console.WriteLine("===========================================");
+                foreach (Movie movie in movies)
+                {
+                    if (movie.intCategory == category)
+                    {
+                        Console.WriteLine(movie.title);
+                    }
+                }
+                Console.WriteLine("Would you like to look up another category? y/n: ");
+                Continue = Console.ReadLine();
+                while (Continue != "y" && Continue != "n")
+                {
+                    Console.WriteLine("Please enter y to continue or n to exit the program: ");
+                    Continue = Console.ReadLine();
+                }
+
+                return Continue;
+            }
+            else
+            {
+                Console.WriteLine("That category does not exist.  Would you like to continue? y/n: ");
+                Continue = Console.ReadLine();
+                while (Continue != "y" && Continue != "n")
+                {
+                    Console.WriteLine("Please enter y to continue or n to exit the program: ");
+                    Continue = Console.ReadLine();
+                }
+
+                return Continue;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10_MovieApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userCategorySelect;
+            string userContinue = "y";
+            //bool userContinue;
+
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie("Terminator", "Scifi", 1), new Movie("Terminator II", "Scifi", 1), new Movie("Terminator III Rise of the Machines", "Scifi", 1),
+                new Movie("Lost in Space", "Scifi", 1), new Movie("Texas Chainsaw Massacre", "Horror", 2), new Movie("Childs Play", "Horror", 2),
+                new Movie("Holloween", "Horror", 2), new Movie("Jeepers Creeprs", "Horror", 2), new Movie("Power Rangers", "Scifi", 1),
+                new Movie("The Great Mouse Detective", "Animated", 3), new Movie("Rocky I", "Drama", 4), new Movie("Rocky II", "Drama", 4),
+                new Movie("Rocky III", "Drama", 4), new Movie("Rocky IV", "Drama", 4), new Movie("Rocky V", "Drama", 4), new Movie("Rocky Balboa", "Drama", 4),
+                new Movie("Creed I", "Drama", 4), new Movie("Creed II", "Drama", 4), new Movie("Good Will Hunting", "Drama", 4)
+            };
+
+            while(userContinue == "y")
+            {
+                Movie.PrintCategories(movies);
+                userCategorySelect = GetUserInput($"ENTER A NUMBER THAT CORRESPONDS TO THE ABOVE CATEGORIES TO VIEW A LIST OF AVAILABLE MOVIES: ", movies.Count);
+                userContinue = Movie.PrintMovies(4, userCategorySelect, movies);
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            string userInput;
+            Console.WriteLine(message);
+            userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        public static int GetUserInput(string message, int i)
+        {
+            string userInput;
+            Console.WriteLine(message);
+            userInput = Console.ReadLine();
+            int value = ValidateUserInput(userInput, i);
+
+            return value;
+        }
+
+        public static int ValidateUserInput(string userInput, int categoryNum)
+        {
+            int categoryOption;
+
+            try
+            {
+                categoryOption = int.Parse(userInput);
+                //return categoryOption;
+            }
+
+            catch (FormatException)
+            {
+                return ValidateUserInput(GetUserInput("Could not parse your entry to an int.  Make sure you're entering in a number"), categoryNum);
+            }
+            if (categoryOption >= 1 && categoryOption <= categoryNum)
+            {
+                return categoryOption;
+            }
+            else
+            {
+                return ValidateUserInput(GetUserInput("Value is not a valid option.  Please select a number between 1 and {categoryNum}"), categoryNum);
+            }
+
+        }
+    }
+}
